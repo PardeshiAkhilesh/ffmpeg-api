69bcfdd45a19d836d9239db6ccb31e7a6a91f905
PardeshiAkhilesh <akhileshpardeshi3@gmail.com>
2025-07-12 00:03:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,14 @@ def home():
 def extract_audio():
     video = request.files['video']
     video.save("input.mp4")
-    subprocess.run(['ffmpeg', '-i', 'input.mp4', '-vn', '-acodec', 'pcm_s16le', '-ar', '16000', '-ac', '1', 'audio.wav'])
+    subprocess.run([
+        'ffmpeg', '-i', 'input.mp4',
+        '-vn', '-acodec', 'pcm_s16le',
+        '-ar', '16000', '-ac', '1', 'audio.wav'
+    ])
     return send_file("audio.wav", as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
