a8eec3a3111f258d1184e179d73a46d881d741a7
PardeshiAkhilesh <akhileshpardeshi3@gmail.com>
2025-07-11 23:18:02 +0530

    Initial commit: added Flask FFmpeg API files

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, send_file
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "FFmpeg API is running!"
+
+@app.route('/extract-audio', methods=['POST'])
+def extract_audio():
+    video = request.files['video']
+    video.save("input.mp4")
+    subprocess.run(['ffmpeg', '-i', 'input.mp4', '-vn', '-acodec', 'pcm_s16le', '-ar', '16000', '-ac', '1', 'audio.wav'])
+    return send_file("audio.wav", as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
